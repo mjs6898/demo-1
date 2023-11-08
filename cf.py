4159f64461a615d64d5a9c54282bf794d75ad379
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-11-08 11:57:42 +0530

    Create cf.py

---
+++ cf.py
@@ -0,0 +1,22 @@
+import boto3
+import sys
+ 
+# Define the stack name with a timestamp
+stack_name = f"SimpleS3Stack-{int(time.time())}"
+ 
+# Initialize Boto3 CloudFormation client
+cf_client = boto3.client('cloudformation')
+ 
+# Get the path to the CloudFormation template file from the command line arguments
+template_file_path = sys.argv[1]
+ 
+try:
+    # Create the CloudFormation stack using the provided template file
+    response = cf_client.create_stack(
+        StackName=stack_name,
+        TemplateBody=open(template_file_path, 'r').read(),
+        Capabilities=['CAPABILITY_IAM']
+    )
+    print(f"S3 stack {stack_name} creation initiated. StackId: {response['StackId']}")
+except Exception as e:
+    print(f"Error creating S3 stack: {str(e)}")
