b84b46bb12b840a08b7b554a77b0e1e39b4e84ba
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-12-01 14:11:22 +0530

    Create test-cp.py

---
+++ test-cp.py
@@ -0,0 +1,38 @@
+import boto3
+import time
+import os
+
+def assume_member_account_role(account_id):
+    # Read temporary credentials from readonly role
+    sts = boto3.client("sts")
+
+    try:
+        response = sts.assume_role(
+            RoleArn=f"arn:aws:iam::{account_id}:role/CFNDeployRole",
+            RoleSessionName=f"cfn-{account_id}"
+        )
+        temp_credentials = response['Credentials']
+        # Use the temporary credentials to create a new session
+        target_session = boto3.Session(
+            aws_access_key_id=temp_credentials['AccessKeyId'],
+            aws_secret_access_key=temp_credentials['SecretAccessKey'],
+            aws_session_token=temp_credentials['SessionToken'],
+            region_name='ap-southeast-2'
+        )
+        return target_session
+    except Exception as e:
+        print(f"Error assuming role in account {account_id}: {str(e)}")
+        return None
+
+# Get target account list from env var
+account_input_string = os.getenv("ACCOUNT_ID_LIST")
+account_output_string = account_input_string.replace(" ","")
+account_list = account_output_string.split(',')
+
+for account_id in account_list:
+    assumed_session = assume_member_account_role(account_id)
+    if assumed_session:
+       print('ASSUMED ROLE')
+    else:
+        print(f"Unable to create or update in account due to assume role issue")
+   
