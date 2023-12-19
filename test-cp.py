d66d9329a7ed998ca6179f0faae5ac0fd50865fe
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-12-19 10:46:14 +0530

    Update test-cp.py

--- test-cp.py
+++ test-cp.py
@@ -1,6 +1,16 @@
 import boto3
 import time
+import sys
 import os
+
+
+stack_name = f"SimpleS3Stack-{int(time.time())}"
+
+ 
+# Initialize Boto3 CloudFormation client
+
+
+template_file_path = sys.argv[1]
 
 def assume_member_account_role(account_id):
     # Read temporary credentials from readonly role
@@ -23,6 +33,23 @@ def assume_member_account_role(account_id):
     except Exception as e:
         print(f"Error assuming role in account {account_id}: {str(e)}")
         return None
+def create_resource(account_id):
+
+    cf_client = boto3.client('cloudformation')
+
+    try:
+    # Create the CloudFormation stack using the provided template file
+        response = cf_client.create_stack(
+            StackName=stack_name,
+            TemplateBody=open(template_file_path, 'r').read(),
+            Capabilities=['CAPABILITY_IAM']
+        )
+        print(f"S3 stack {stack_name} creation initiated. StackId: {response['StackId']}")
+        return response
+    except Exception as e:
+        print(f"Error creating S3 stack: {str(e)}")
+        return None
+
 
 # Get target account list from env var
 account_input_string = os.getenv("ACCOUNT_ID_LIST")
@@ -35,4 +62,10 @@ for account_id in account_list:
        print('ASSUMED ROLE')
     else:
         print(f"Unable to create or update in account due to assume role issue")
+
+    resources_created = create_resource(account_id)
+    if resources_created:
+        print("resource creation is success")
+    else:
+        print("resource creation failed")
    
