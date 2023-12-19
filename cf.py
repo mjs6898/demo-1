676c8dc7f5092ec557c4aa9828efaaccea38ec51
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-12-19 11:24:03 +0530

    Update cf.py

--- cf.py
+++ cf.py
@@ -1,26 +1,111 @@
 import boto3
-import sys
 import time
- 
-# Define the stack name with a timestamp
+import sys
+import os
+
+
 stack_name = f"SimpleS3Stack-{int(time.time())}"
-# stack_name = "python-cf-stack"
+
  
 # Initialize Boto3 CloudFormation client
-cf_client = boto3.client('cloudformation')
- 
-# Get the path to the CloudFormation template file from the command line arguments
-template_file_path = sys.argv[1]
- 
-try:
-    # Create the CloudFormation stack using the provided template file
-    response = cf_client.create_stack(
-        StackName=stack_name,
-        TemplateBody=open(template_file_path, 'r').read(),
-        Capabilities=['CAPABILITY_IAM']
-    )
-    print(f"S3 stack {stack_name} creation initiated. StackId: {response['StackId']}")
-except Exception as e:
-    print(f"Error creating S3 stack: {str(e)}")
 
-#........
+
+template_file_path = sys.argv[1]
+
+def assume_member_account_role(account_id):
+    # Read temporary credentials from readonly role
+    sts = boto3.client("sts")
+
+    try:
+        response = sts.assume_role(
+            RoleArn="arn:aws:iam::178273572362:role/Assume-role-test-4",
+            RoleSessionName=f"cfn-{account_id}"
+        )
+        temp_credentials = response['Credentials']
+        # Use the temporary credentials to create a new session
+        target_session = boto3.Session(
+            aws_access_key_id=temp_credentials['AccessKeyId'],
+            aws_secret_access_key=temp_credentials['SecretAccessKey'],
+            aws_session_token=temp_credentials['SessionToken'],
+            region_name='ap-southeast-2'
+        )
+        return target_session
+    except Exception as e:
+        print(f"Error assuming role in account {account_id}: {str(e)}")
+        return None
+
+def create_update_stack(stack_name, assumed_session, account_id):
+    # Check if the stack already exists
+
+    cf_client = assumed_session.client('cloudformation')
+    try:
+        cf_client.describe_stacks(StackName=stack_name)
+        stack_exists = True
+    except cf_client.exceptions.ClientError as e:
+        if 'does not exist' in str(e):
+            stack_exists = False
+        else:
+            print(f"Error checking stack {stack_name} existence in account {account_id}: {str(e)}")
+
+
+    # Read the template file content
+    try:
+        with open(template_file_path, 'r') as template_file:
+            template_body = template_file.read()
+    except Exception as e:
+        print(f"Error reading template file in account {account_id}: {str(e)}")
+
+
+    # If the stack exists, update it; otherwise, create it
+    if stack_exists:
+        try:
+            # Update the stack
+            cf_client.update_stack(
+                StackName=stack_name,
+                TemplateBody=template_body,
+                Capabilities=['CAPABILITY_NAMED_IAM','CAPABILITY_IAM']
+            )
+            print(f'Updating stack {stack_name} in account {account_id}')
+        except cf_client.exceptions.ClientError as e:
+            print(f"Error updating stack {stack_name} in account {account_id}: {str(e)}")
+    else:
+        try:
+            # Create the stack
+            cf_client.create_stack(
+                StackName=stack_name,
+                TemplateBody=template_body,
+                Capabilities=['CAPABILITY_NAMED_IAM','CAPABILITY_IAM']
+            )
+            print(f'Creating stack {stack_name} in account {account_id}')
+        except cf_client.exceptions.ClientError as e:
+            print(f"Error creating stack {stack_name} in account {account_id}: {str(e)}")
+
+
+
+# Get target account list from env var
+account_input_string = os.getenv("ACCOUNT_ID_LIST")
+account_output_string = account_input_string.replace(" ","")
+account_list = account_output_string.split(',')
+
+success_accounts = []
+failed_accounts = []
+
+
+for account_id in account_list:
+    assumed_session = assume_member_account_role(account_id)
+    if assumed_session:
+        create_update_stack(stack_name, assumed_session, account_id)
+    else:
+        print(f"Unable to create or update {stack_name} in account {account_id} due to assume role issue")
+        failed_accounts.append(account_id)
+
+# # Check cloudformation stack status
+# for account_id in account_list:
+#     assumed_session = assume_member_account_role(account_id)
+#     if assumed_session:
+#         check_stack_status(stack_name, assumed_session, account_id)
+
+print(f"Stack deployment completed successfully for those accounts: {success_accounts}")
+print(f"Stack deployment failed for those accounts: {failed_accounts}")
+
+   
