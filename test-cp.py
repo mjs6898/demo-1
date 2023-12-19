6a2a10cc492f11561aa6c4d426babbac12a98a3f
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-12-19 14:35:19 +0530

    Update test-cp.py

--- test-cp.py
+++ test-cp.py
@@ -21,7 +21,7 @@ def create_cloudformation_stack(stack_name, template_file, credentials):
         template_body = file.read()
  
     response = cloudformation_client.create_stack(
-        StackName=stack_name,
+        StackName=CP-1,
         TemplateBody=template_body,
         Capabilities=['CAPABILITY_NAMED_IAM']
     )
