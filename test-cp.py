06b77b6321265503ef1f3e684a6088e388335932
mjs6898 <146416870+mjs6898@users.noreply.github.com>
2023-12-22 11:21:36 +0530

    Update test-cp.py

--- test-cp.py
+++ test-cp.py
@@ -16,7 +16,7 @@ def assume_member_account_role(account_id):
 
     try:
         response = sts.assume_role(
-            RoleArn=f"arn:aws:iam::682665177220:role/assume-role-2",
+            RoleArn=f"arn:aws:iam::{account_id}:role/assume-role-2",
             RoleSessionName=f"cfn-{account_id}"
         )
         print(account_id)
